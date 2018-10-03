f5d53884e2dfe92fe9baba6b9e6db5aa104e9a10
Nick Junius <njunius@users.noreply.github.com>
2018-10-03 12:26:50 -0700

    Added Print while writing File

--- P1/p1_dumb_gen.py
+++ P1/p1_dumb_gen.py
@@ -1,39 +1,41 @@
 import random
+from random import randint
 import sys
 def gen_dungeon(filename, width, height):
-	dungeon_line = ""
-	dungeon_level = []
-	for row in range(0, height):
-		dungeon_level.append([])
-	
-	dungeon_file = open(filename, "w")
-	for row in range(0, height):
-		for col in range(0, width):
-			if random.random() < 0.7:
-				dungeon_level[col].append('.')
-			else:
-				dungeon_level[col].append('X')
-	
-	random_waypoint_x = random.randint(0, width - 1)
-	random_waypoint_y = random.randint(0, height - 1)
-	dungeon_level[random_waypoint_x][random_waypoint_y] = 'a'
-	
-	random_waypoint_x = random.randint(0, width - 1)
-	random_waypoint_y = random.randint(0, height - 1)
+    dungeon_line = ""
+    dungeon_level = []
+    for row in range(0, height):
+        dungeon_level.append([])
+    
+    dungeon_file = open(filename, "w")
+    for row in range(0, height):
+        for col in range(0, width):
+            if random.random() < 0.7:
+                dungeon_level[col].append(str(randint(1, 9)))
+            else:
+                dungeon_level[col].append('X')
+    
+    random_waypoint_x = random.randint(0, width - 1)
+    random_waypoint_y = random.randint(0, height - 1)
+    dungeon_level[random_waypoint_x][random_waypoint_y] = 'a'
+    
+    random_waypoint_x = random.randint(0, width - 1)
+    random_waypoint_y = random.randint(0, height - 1)
 
-	dungeon_level[random_waypoint_x][random_waypoint_y] = 'b'
+    dungeon_level[random_waypoint_x][random_waypoint_y] = 'e'
 
-	for row in range(0, height):
-		dungeon_level[row].append('\n')
-		dungeon_line = "".join(dungeon_level[row])
-		dungeon_file.write(dungeon_line)
-	
-	dungeon_file.close()
+    for row in range(0, height):
+        dungeon_level[row].append('\n')
+        dungeon_line = "".join(dungeon_level[row])
+        print(dungeon_line[:-2])
+        dungeon_file.write(dungeon_line)
+    
+    dungeon_file.close()
 
 
 if __name__ == '__main__':
-	import sys
-	_, filename, width, height = sys.argv
-	int_width = int(width)
-	int_height = int(height)
-	gen_dungeon(filename, int_width, int_height)
+    import sys
+    _, filename, width, height = sys.argv
+    int_width = int(width)
+    int_height = int(height)
+    gen_dungeon(filename, int_width, int_height)
